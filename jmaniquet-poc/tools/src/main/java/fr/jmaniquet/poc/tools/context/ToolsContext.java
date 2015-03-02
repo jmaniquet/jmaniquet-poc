668686d87aa4217a138544b1bd10078f28d38f89
jmaniquet <jmaniquet@norsys.fr>
2015-03-02 16:50:33 +0100

    Adding basic annotation-based context

---
+++ jmaniquet-poc/tools/src/main/java/fr/jmaniquet/poc/tools/context/ToolsContext.java
@@ -0,0 +1,9 @@
+package fr.jmaniquet.poc.tools.context;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("fr.jmaniquet.poc.tools")
+public class ToolsContext {
+}
