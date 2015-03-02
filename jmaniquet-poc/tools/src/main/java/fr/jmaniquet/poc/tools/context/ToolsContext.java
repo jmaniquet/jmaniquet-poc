360b25efac7b0a8893ae5d57ea28f3015a582125
jmaniquet <jmaniquet@norsys.fr>
2015-03-02 17:10:16 +0100

    Adding basic blocks for DB based utilities : jdbcTemplate with basic schema

--- jmaniquet-poc/tools/src/main/java/fr/jmaniquet/poc/tools/context/ToolsContext.java
+++ jmaniquet-poc/tools/src/main/java/fr/jmaniquet/poc/tools/context/ToolsContext.java
@@ -1,9 +1,22 @@
 package fr.jmaniquet.poc.tools.context;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 @ComponentScan("fr.jmaniquet.poc.tools")
 public class ToolsContext {
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		return new JdbcTemplate(dataSource);
+	}
 }
--- jmaniquet-poc/tools/src/test/java/fr/jmaniquet/poc/tools/context/ToolsContextTest.java
+++ jmaniquet-poc/tools/src/test/java/fr/jmaniquet/poc/tools/context/ToolsContextTest.java
@@ -1,9 +1,12 @@
 package fr.jmaniquet.poc.tools.context;
 
+import javax.sql.DataSource;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -13,7 +16,23 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class ToolsContextTest extends AbstractJUnit4SpringContextTests {
 	
 	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
 	private ToolsContext toolsContext;
+	
+	@Test
+	public void testDataSourceNotNull() {
+		Assert.assertNotNull(dataSource);
+	}
+	
+	@Test
+	public void testJdbcTemplateNotNull() {
+		Assert.assertNotNull(jdbcTemplate);
+	}
 	
 	@Test
 	public void testToolsContextNotNull() {
