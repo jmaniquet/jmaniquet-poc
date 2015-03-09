f35b004e4bddd0311a6cc943ab560d96da7fa752
jmaniquet <jmaniquet@norsys.fr>
2015-03-09 18:31:26 +0100

    Adding support of JodaTime directly in JdbcTemplate.

---
+++ jmaniquet-poc/jdbctemplate-jodatime/src/test/java/fr/jmaniquet/poc/jdbctemplate/jodatime/JodaJdbcTemplateInsertTest.java
@@ -0,0 +1,68 @@
+package fr.jmaniquet.poc.jdbctemplate.jodatime;
+
+import org.joda.time.DateTime;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import fr.jmaniquet.poc.tools.random.RandomUtils;
+import fr.jmaniquet.poc.tools.user.User;
+import fr.jmaniquet.poc.tools.user.UserBuilder;
+import fr.jmaniquet.poc.tools.user.UserUtils;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring/jdbctemplate-jodatime-test-context.xml"})
+public class JodaJdbcTemplateInsertTest extends AbstractTransactionalJUnit4SpringContextTests {
+	
+	private static final String SQL_INSERT_ALL_FIELDS = "INSERT INTO USERS (ID, NAME, GIVEN_NAME, BIRTH_DATE) VALUES (?, ?, ?, ?)";
+	private static final String SQL_INSERT_WITH_DEFAULT_BIRTHDATE = "INSERT INTO USERS (ID, NAME, GIVEN_NAME) VALUES (?, ?, ?)";
+	
+	@Autowired
+	@Qualifier(BeanConstants.CUSTOM_JDBCTEMPLATE)
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	private UserUtils userUtils;
+	
+	private long id = RandomUtils.randomId();
+	private String name = "fakeName";
+	private String givenName = "fakeGivenName";
+	private DateTime birthDate = RandomUtils.randomDate();
+	
+	@Test
+	public void testInsert() {
+		User expectedUser = UserBuilder.builder().id(id).name(name).givenName(givenName).birthDate(birthDate).build();
+		jdbcTemplate.update(SQL_INSERT_ALL_FIELDS, id, name, givenName, birthDate);
+		
+		User actualUser = userUtils.findUserById(id);
+		Assert.assertNotNull(actualUser.getBirthDate());
+		Assert.assertEquals(birthDate, actualUser.getBirthDate());
+		userUtils.assertEquals(expectedUser, actualUser);
+	}
+	
+	@Test
+	public void testInsertWithBirthDateNullDefault() {
+		User expectedUser = UserBuilder.builder().id(id).name(name).givenName(givenName).birthDate(null).build();
+		jdbcTemplate.update(SQL_INSERT_WITH_DEFAULT_BIRTHDATE, id, name, givenName);
+		
+		User actualUser = userUtils.findUserById(id);
+		Assert.assertNull(actualUser.getBirthDate());
+		userUtils.assertEquals(expectedUser, actualUser);
+	}
+	
+	@Test
+	public void testInsertWithBirthDateNullParam() {
+		User expectedUser = UserBuilder.builder().id(id).name(name).givenName(givenName).birthDate(null).build();
+		jdbcTemplate.update(SQL_INSERT_ALL_FIELDS, id, name, givenName, null);
+		
+		User actualUser = userUtils.findUserById(id);
+		Assert.assertNull(actualUser.getBirthDate());
+		userUtils.assertEquals(expectedUser, actualUser);
+	}
+}
