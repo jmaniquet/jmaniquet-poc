be5bb9c4299d74301a07ea0b33bc56e093a6bb12
jmaniquet <jmaniquet@norsys.fr>
2015-03-11 14:09:28 +0100

    Adding test coverage for JdbcTemplate.update with DateTime

---
+++ jmaniquet-poc/jdbctemplate-jodatime/src/test/java/fr/jmaniquet/poc/jdbctemplate/jodatime/JodaJdbcTemplateUpdateTest.java
@@ -0,0 +1,77 @@
+package fr.jmaniquet.poc.jdbctemplate.jodatime;
+
+import static fr.jmaniquet.poc.tools.constants.TestDataConstants.USER1_BIRTHDATE;
+import static fr.jmaniquet.poc.tools.constants.TestDataConstants.USER1_ID;
+import static fr.jmaniquet.poc.tools.constants.TestDataConstants.USER2_ID;
+
+import org.joda.time.DateTime;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+
+import fr.jmaniquet.poc.tools.random.RandomUtils;
+import fr.jmaniquet.poc.tools.user.User;
+import fr.jmaniquet.poc.tools.user.UserUtils;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring/jdbctemplate-jodatime-test-context.xml"})
+@TestExecutionListeners(listeners = DbUnitTestExecutionListener.class)
+@DbUnitConfiguration(databaseConnection = "dataSource")
+@DatabaseSetup("classpath:/selectuser-dataset.xml")
+public class JodaJdbcTemplateUpdateTest extends AbstractTransactionalJUnit4SpringContextTests {
+	
+	private static final String SQL_UPDATE_BIRTHDATE_BY_ID = "UPDATE USERS SET BIRTH_DATE = ? WHERE ID = ?";
+	
+	@Autowired
+	@Qualifier(BeanConstants.CUSTOM_JDBCTEMPLATE)
+	private JdbcTemplate underTest;
+	
+	@Autowired
+	private UserUtils userUtils;
+	
+	@Test
+	public void testUpdateBirthDate() {
+		checkBirthDate(USER1_ID, USER1_BIRTHDATE);
+		DateTime newDate = RandomUtils.randomDate();
+		underTest.update(SQL_UPDATE_BIRTHDATE_BY_ID, newDate, USER1_ID);
+		checkBirthDate(USER1_ID, newDate);
+	}
+	
+	@Test
+	public void testUpdateBirthDateNotNullToNull() {
+		checkBirthDate(USER1_ID, USER1_BIRTHDATE);
+		underTest.update(SQL_UPDATE_BIRTHDATE_BY_ID, null, USER1_ID);
+		checkBirthDate(USER1_ID, null);
+	}
+	
+	@Test
+	public void testUpdateBirthDateNullToNotNull() {
+		checkBirthDate(USER2_ID, null);
+		DateTime newDate = RandomUtils.randomDate();
+		underTest.update(SQL_UPDATE_BIRTHDATE_BY_ID, newDate, USER2_ID);
+		checkBirthDate(USER2_ID, newDate);
+	}
+	
+	private void checkBirthDate(Long userId, DateTime expectedBirthDate) {
+		User u = userUtils.findUserById(userId);
+		DateTime actual = u.getBirthDate();
+		
+		if (expectedBirthDate == null) {
+			Assert.assertNull(actual);
+		} else {
+			Assert.assertEquals(expectedBirthDate, actual);
+		}
+		
+	}
+}
