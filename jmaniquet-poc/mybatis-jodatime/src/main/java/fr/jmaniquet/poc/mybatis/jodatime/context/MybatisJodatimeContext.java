d30fa078fb76b6e155cde32f2dc3e45244a5a4a8
jmaniquet <jmaniquet@norsys.fr>
2015-03-11 15:03:02 +0100

    Cleaning declaration of TransactionManager : it is now declared by the Tools module. Side effect : fixes BeanNotFoundException thrown by the JdbcTemplate module.

--- jmaniquet-poc/mybatis-jodatime/src/main/java/fr/jmaniquet/poc/mybatis/jodatime/context/MybatisJodatimeContext.java
+++ jmaniquet-poc/mybatis-jodatime/src/main/java/fr/jmaniquet/poc/mybatis/jodatime/context/MybatisJodatimeContext.java
@@ -10,16 +10,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.support.ResourceTransactionManager;
 
 import fr.jmaniquet.poc.mybatis.jodatime.mapper.GenericMapper;
 import fr.jmaniquet.poc.tools.context.ToolsContext;
 
 @Configuration
 @Import(ToolsContext.class)
-@EnableTransactionManagement(order = 1)
 @MapperScan(
 		basePackages = {"fr.jmaniquet.poc.mybatis.jodatime.mapper"},
 		markerInterface=GenericMapper.class,
@@ -40,10 +36,5 @@ public class MybatisJodatimeContext {
 		/*Resource[] resources = new PathMatchingResourcePatternResolver().getResources("classpath*:fr/jmaniquet/poc/*Mapper.xml");
 		sqlSessionFactoryBean.setMapperLocations(resources);*/
 		return sqlSessionFactoryBean.getObject();
-	}
-	
-	@Bean
-	public ResourceTransactionManager transactionManager() {
-		return new DataSourceTransactionManager(dataSource);
 	}
 }
