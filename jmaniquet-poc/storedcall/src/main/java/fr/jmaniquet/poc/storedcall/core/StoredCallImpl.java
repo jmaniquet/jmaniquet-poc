0d9284f15d5b484b8f420f50b5d3733bf16fb658
jmaniquet <jmaniquet@norsys.fr>
2015-04-25 14:13:36 +0200

    Converting cursorParameters property from List to Array

--- jmaniquet-poc/storedcall/src/main/java/fr/jmaniquet/poc/storedcall/core/StoredCallImpl.java
+++ jmaniquet-poc/storedcall/src/main/java/fr/jmaniquet/poc/storedcall/core/StoredCallImpl.java
@@ -1,7 +1,5 @@
 package fr.jmaniquet.poc.storedcall.core;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
@@ -18,14 +16,14 @@ public class StoredCallImpl implements StoredCall {
 	
 	private String procedureName;
 	private SqlParameter [] parameters;
-	private List<CursorParameter<?>> cursorParameters;
+	private CursorParameter<?> [] cursorParameters;
 	private JdbcTemplate jdbcTemplate;
 
 	private SimpleJdbcCall call;
 	
 	public StoredCallImpl() {
 		parameters = new SqlParameter[0];
-		cursorParameters = new ArrayList<>();
+		cursorParameters = new CursorParameter<?>[0];
 	}
 	
 	StoredCallImpl(SimpleJdbcCall call) {
@@ -88,11 +86,11 @@ public class StoredCallImpl implements StoredCall {
 		this.parameters = parameters;
 	}
 
-	public List<CursorParameter<?>> getCursorParameters() {
+	public CursorParameter<?> [] getCursorParameters() {
 		return cursorParameters;
 	}
 
-	public void setCursorParameters(List<CursorParameter<?>> cursorParameters) {
+	public void setCursorParameters(CursorParameter<?> ... cursorParameters) {
 		this.cursorParameters = cursorParameters;
 	}
 	
